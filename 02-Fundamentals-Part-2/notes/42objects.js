5c0b7bd7b42948b675df2751dd2738aadb1c23ed
cjwbrandon <cjwbrandon@gmail.com>
2020-11-22 16:15:01 +0800

    [ADD] Intro to objects

---
+++ 02-Fundamentals-Part-2/notes/42objects.js
@@ -0,0 +1,18 @@
+// Data Structure: Objects
+// Idea: Giving elements a name
+// Key-value pairs
+
+// Object Literal Syntax
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+// Keys are also called Properties
+// jonas have 5 Properties
+
+// Note: For Objects, the order of the Elements does not matter at all when we retrieve them.
+
+// Array -> More structure, Objects -> More unstructured
