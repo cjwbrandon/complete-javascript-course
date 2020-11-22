10801a764a6850806b7561cbe06fdb61e9a8756b
cjwbrandon <cjwbrandon@gmail.com>
2020-11-22 14:28:57 +0800

    [ADD] Calling functions within a function

---
+++ 02-Fundamentals-Part-2/notes/36functionsCallingOthers.js
@@ -0,0 +1,20 @@
+// Functions Calling Other Functions
+
+// Example
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  // Calling another function inside a function
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+// Why? DRY
+// Much easier to change parts of the code
+// Example: Changing the number of pieces
