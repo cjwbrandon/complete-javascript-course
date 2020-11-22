10649029a16b55c36f1e6cade923170591dfa4af
cjwbrandon <cjwbrandon@gmail.com>
2020-11-22 16:57:08 +0800

    [ADD] For loop

---
+++ 02-Fundamentals-Part-2/notes/46forLoop.js
@@ -0,0 +1,19 @@
+'use-strict';
+
+// Control Structure
+// Iterations: The For Loop
+// Allows you to do repetitions
+
+// Have to repeat for each
+console.log('Lifting weights repeitition 1');
+console.log('Lifting weights repeitition 2');
+console.log('Lifting weights repeitition 3');
+// ....
+
+// For Loop Example
+// for (let {counterVariable}; {condition}; {updateCounter})
+// counterVariable: Must use let because the Variable will be changed later
+// condition: condition to stop -> for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
