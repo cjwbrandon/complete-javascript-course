e24fa54842ff8dfbdd011a60f890b438a4b29055
cjwbrandon <cjwbrandon@gmail.com>
2020-12-03 08:26:47 +0800

    [ADD] Section 11: Coding Challenge 3

--- 11-Arrays-Bankist/153codingChallenge3/script.js
+++ 11-Arrays-Bankist/153codingChallenge3/script.js
@@ -11,3 +11,16 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+
+const calcAverageHumanAge = function (ages) {
+  // Task 1
+  const average = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, _, arr) => (acc + age) / arr.length, 0);
+  return average;
+};
+
+// Task 4
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
