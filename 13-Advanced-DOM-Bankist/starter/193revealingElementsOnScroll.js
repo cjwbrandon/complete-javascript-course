9b9f8d9c13ef0dd128ec09cae9a3beb10d9ce923
cjwbrandon <cjwbrandon@gmail.com>
2020-12-06 16:57:17 +0800

    [ADD] Revealing Elements on Scroll

---
+++ 13-Advanced-DOM-Bankist/starter/193revealingElementsOnScroll.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Revealing Elements on Scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectinoObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observer(section);
+  section.classList.add('section--hidden');
+});
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -120,3 +120,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observer(header);
+
+///////////////////////////////////////
+// Revealing Elements on Scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectinoObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observer(section);
+  section.classList.add('section--hidden');
+});
