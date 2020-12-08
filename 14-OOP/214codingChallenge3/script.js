55adc8ef23a11d7b69aac4b21a58881546457b68
cjwbrandon <cjwbrandon@gmail.com>
2020-12-08 08:25:05 +0800

    [ADD] Section 14: Coding Challenge 3

--- 14-OOP/214codingChallenge3/script.js
+++ 14-OOP/214codingChallenge3/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
 ///////////////////////////////////////
 // Coding Challenge #3
 
@@ -13,3 +28,32 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
 */
+
+// Task 1
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+
+// Task 2
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+// Task 3
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+// Task 4
+const tesla = new Tesla('Tesla', 120, 23);
+
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(50);
