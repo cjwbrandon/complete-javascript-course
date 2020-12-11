bf181837074d95bbb3a39986298ac567cfabef0e
cjwbrandon <cjwbrandon@gmail.com>
2020-12-11 10:44:06 +0800

    [ADD] Welcome to Callback Hell

---
+++ 16-Asynchronous/starter/242XMLHttpRequest copy.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// Welcome to Callback Hell
+// Nested callbacks
+// Executing Asyncrhonous code in sequence
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  // .open({method}, {url})
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+
+  // Asynchronous -> Cannot directly assign to a variable
+  request.send();
+  // console.log(request.responseText); // empty
+  // Instead have to use an event listener
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('singapore');
+// getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+// Issue: Callback Hell is Messy making it hard to maintain and reason about
+// Introduces more bugs and is bad
+// CODE THAT IS HARD TO UNDERSTAND IS BAD CODE
+// More bugs and harder to add functionality (build upon)
