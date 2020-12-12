aaeae252907e61b2ccb012ee392c338eb74cf445
cjwbrandon <cjwbrandon@gmail.com>
2020-12-12 20:18:02 +0800

    [ADD] CommonJS Modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,79 +3,91 @@
 // Importing module
 // Import Name Exports
 // Note: you can change the name of the Variables inported
-import {
-  addToCart,
-  totalPrice as price,
-  totalQuantity,
-} from './shoppingCart.js';
-
-// Cannot use import statement outside a module
-// Remember to specify the type attribute in HTML linking
-console.log('Importing module');
-// Note: the Exporting module is executed before the imported module
-
-addToCart('bread', 5);
-console.log(totalPrice, totalQuantity);
-
-// Importing everything -> Creates a namespace for it
-import * as ShoppingCart from './shoppingCart.js';
-
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
-
-// Importing default exports
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-
-// Mixing default and named exports -> Not desireable to mix exports
-// import add, {
+// import {
 //   addToCart,
 //   totalPrice as price,
 //   totalQuantity,
 // } from './shoppingCart.js';
-// console.log(price);
 
-// Live connection -> Not copies -> Points to the same place in memory
-add('bread', 5);
-add('apples', 4);
+// // Cannot use import statement outside a module
+// // Remember to specify the type attribute in HTML linking
+// console.log('Importing module');
+// // Note: the Exporting module is executed before the imported module
 
-// cart is manipulated by the add function
-// is not simply a copy since cart is an empty array
-console.log(cart);
+// addToCart('bread', 5);
+// console.log(totalPrice, totalQuantity);
 
-// Video 267: The Module Pattern
-// Main goal of module pattern is to encapsulate functionality, have Private data and to expose a Public API
-// Best way is to use Functions -> Private data & return values (Public API)
-// Usually write an IFFE
-const ShoppingCart2 = (function () {
-  // Private data
-  const cart = [];
-  const shipppingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// // Importing everything -> Creates a namespace for it
+// import * as ShoppingCart from './shoppingCart.js';
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
 
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+// // Importing default exports
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
 
-  // return -> Public API
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
+// // Mixing default and named exports -> Not desireable to mix exports
+// // import add, {
+// //   addToCart,
+// //   totalPrice as price,
+// //   totalQuantity,
+// // } from './shoppingCart.js';
+// // console.log(price);
 
-// Note: Works because of closures
-// Note: We cannot access anything in the module from the console due to the scope
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 4);
-console.log(ShoppingCart2);
-// Private property
-console.log(ShippingCart2.shipppingCost);
+// // Live connection -> Not copies -> Points to the same place in memory
+// add('bread', 5);
+// add('apples', 4);
+
+// // cart is manipulated by the add function
+// // is not simply a copy since cart is an empty array
+// console.log(cart);
+
+// // Video 267: The Module Pattern
+// // Main goal of module pattern is to encapsulate functionality, have Private data and to expose a Public API
+// // Best way is to use Functions -> Private data & return values (Public API)
+// // Usually write an IFFE
+// const ShoppingCart2 = (function () {
+//   // Private data
+//   const cart = [];
+//   const shipppingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   // return -> Public API
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// // Note: Works because of closures
+// // Note: We cannot access anything in the module from the console due to the scope
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 4);
+// console.log(ShoppingCart2);
+// // Private property
+// console.log(ShippingCart2.shipppingCost);
+
+// Video 268: CommonJS Modules
+// Used in Node.js
+// Most repos in npm is implemented in CommonJS
+// Note: Won't work in the browser but will in Node.js -> export is not defined in the browser but is in Node.js
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+// Import
+const { addToCart } = require('./shoppingCart.js')
